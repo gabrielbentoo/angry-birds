ddc5fad0602aa22a697f59c0f3713c636f422fe8
Gabriel Bento <bielnovaes61@gmail.com>
2026-03-31 18:54:51 -0300

    adicionando inimigos

--- sketch.js
+++ sketch.js
@@ -7,7 +7,11 @@ let world;
 let canvas;
 let backgroundImg;
 let ground;
-let platform
+let platform;
+let box1;
+let box2;
+let pig1;
+let log1;
 
 function preload() {
     backgroundImg = loadImage("./assets/bg.png");
@@ -20,6 +24,10 @@ function setup() {
     world = engine.world;
     ground = new Ground(600, height, 1200, 20);
     platform = new Ground(150, 305, 300, 170);
+    box1 = new Box(700, 320, 70, 70);
+    box2 = new Box(920, 320, 70, 70);
+    pig1 = new Pig(810, 350);
+    log1 = new Log(810, 260, 300, PI /2);
 }
 
 function draw() {
@@ -27,6 +35,11 @@ function draw() {
 
     ground.display();
     platform.display();
+    box1.display();
+    box2.display();
+    pig1.display();
+    pig1.score();
+    log1.display();
 
 }
 
